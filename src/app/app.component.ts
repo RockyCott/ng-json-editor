f07738cf93efbb6956956e8dfaed2fa956a1d552
Daniel Felipe Jaimes Blanco <danielvege6@gmail.com>
2024-01-11 20:38:18 -0500

    feat(ace-builds): first test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import 'ace-builds';
+import 'ace-builds/src-noconflict/mode-json';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-json-editor';
+  jsonInputData = '';
+
+  imprimir() {
+    console.log(this.jsonInputData);
+    console.log(this.isJson(this.jsonInputData));
+  }
+
+  // funcion para comprobar si es un json valido
+  isJson(str: string) {
+    try {
+      JSON.parse(str);
+    } catch (e) {
+      return false;
+    }
+    return true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EditorModule } from './editor/editor.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    EditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
